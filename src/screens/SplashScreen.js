e5bd2245545c2a5199c564403173f23c0f61d065
Muhammad Haseeb <104450111+muhammadhaseebb@users.noreply.github.com>
2023-05-20 14:12:20 +0500

    has

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RouteNavigation from './src/navigation/routeNavigation';
+
+function SplashScreen() {
+  return (
+    <View></View>
+  );
+}
+
+export default App;
